0f5dfbb04bc5be6c04d39e826254da27e025c3a7
Abdallah Bari <abdellah01win@gmail.com>
2021-09-23 12:51:36 +0100

    Wrote basic form functunality

---
+++ Scripts/Weather-app.js
@@ -0,0 +1,24 @@
+const country = document.querySelector('#form-country');
+const city = document.querySelector('#form-city');
+
+const worldCities = {
+    Spain : ['Madrid', 'Barcalona', 'Sivilia', 'Granada', 'Valencia'],
+    Italy : ['Milan', 'Rome', 'Vinice', 'Naplis', 'Torine'],
+    Finland : ['Helsinki', 'Osspo', 'Turku', 'Tampere', 'Pori']
+}
+
+country.addEventListener('change', () => {
+    
+    city.option.nextElementSibling.remove(Option);
+    let countryValue = country.options[country.selectedIndex].value;
+        creatNewOption(countryValue)
+})
+
+function creatNewOption(countryValue) {
+    for (const newCity of worldCities[countryValue]) {
+        const newOption = document.createElement('option');
+        newOption.value = newCity;
+        newOption.append(newCity);
+        city.append(newOption);
+    }
+}
