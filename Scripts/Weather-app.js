238cec60f77169f6d1ccda9ec34eff0f025196be
Abdallah Bari <abdellah01win@gmail.com>
2021-09-25 14:52:44 +0100

    Connected to API and Displaied Data in DOM

--- Scripts/Weather-app.js
+++ Scripts/Weather-app.js
@@ -3,21 +3,20 @@ const city = document.querySelector('#form-city');
 const submit = document.querySelector('#submit');
 
 const worldCities = {
-    Spain : ['Madrid', 'Barcalona','Valencia', 'Seville', 'Zaragoza', 'Málaga','Murcia','Bilbao','Alicante','Granada'],
+    Spain: ['Madrid', 'Barcalona', 'Valencia', 'Seville', 'Zaragoza', 'Málaga', 'Murcia', 'Bilbao', 'Alicante', 'Granada'],
 
-    Italy : ['Rome', 'Milan', 'Naples', 'Turin', 'Palermo', 'Genoa', 'Bologna', 'Florence', 'Bari', 'Venice'],
+    Italy: ['Rome', 'Milan', 'Naples', 'Turin', 'Palermo', 'Genoa', 'Bologna', 'Florence', 'Bari', 'Venice'],
 
-    Germany : ['Berlin', 'Hamburg', 'Munich', 'Köln', 'Frankfurt', 'Stuttgart', 'Düsseldorf', 'Dortmund', 'Essen', 'Leipzig'],
+    Germany: ['Berlin', 'Hamburg', 'Munich', 'Köln', 'Frankfurt', 'Stuttgart', 'Düsseldorf', 'Dortmund', 'Essen', 'Leipzig'],
 
-    Norway : ['Oslo', 'Trondheim', 'Drammen', '	Kristiansand', 'Ålesund', 'Tønsberg', 'Moss', 'Haugesund', 'Arendal', 'Hamar'],
+    Norway: ['Oslo', 'Trondheim', 'Drammen', '	Kristiansand', 'Ålesund', 'Tønsberg', 'Moss', 'Haugesund', 'Arendal', 'Hamar'],
 
-    Switzerland : ['Zürich', 'Geneva', 'Basel', 'Lausanne', 'Bern', 'Winterthur', 'Lucerne', 'St. Gallen', 'Lugano', 'Biel/Bienne']
+    Switzerland: ['Zürich', 'Geneva', 'Basel', 'Lausanne', 'Bern', 'Winterthur', 'Lucerne', 'St. Gallen', 'Lugano', 'Biel/Bienne']
 }
 
 country.addEventListener('change', () => {
-    
-    let countryValue = country.options[country.selectedIndex].value;
-        creatNewOption(countryValue)
+    const countryValue = country.options[country.selectedIndex].value;
+    creatNewOption(countryValue)
 })
 
 function creatNewOption(countryValue) {
@@ -29,11 +28,47 @@ function creatNewOption(countryValue) {
     }
 }
 
-submit.addEventListener('click', (e)=>{
+submit.addEventListener('click', (e) => {
     e.preventDefault()
-    const getData = async () =>{
-        const weatherData = await axios.get(`api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=9840f5f10d58e26a6f4365d0c7aa5157`)
-    }
-    console.log(city.value)
+    getData()
+
 })
 
+// Recives data from the API
+
+const getData = async () => {
+    try {
+        const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=cbcafb424d21d0e5649df03b5bb0e352`)
+        displayData(weatherData)
+        //console.log(weatherData.data.weather[0].main)
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+// Displays general weather Status
+
+const displayData = (resp) => {
+    const generalStatus = document.createElement('DIV');
+    generalStatus.setAttribute('id', 'weatherStatus');
+
+    const main = document.createElement('DIV')
+    main.setAttribute('id','main'); 
+
+    main.append(resp.data.weather[0].main);
+    generalStatus.append(main);
+
+    const description = document.createElement('DIV')
+    description.setAttribute('id','description');
+    description.append(resp.data.weather[0].description)
+    generalStatus.append(description);
+
+    const weatherIcon = resp.data.weather[0].icon
+    const icon = document.createElement('img')
+    icon.setAttribute('id','icon');
+    icon.src = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+    generalStatus.append(icon)
+
+    document.body.append(generalStatus)
+}
